d905c33ec416da2d560b74fa1db22dd5cc476daf
RoySaudamini <RoySaudamini@users.noreply.github.com>
2025-10-22 09:34:37 +0000

    deploy: 86c6486e1a2a3163e7b8558437fd51f19ec0de4d

---
+++ assets/Create and Design Digital Products using Canva-CGqcJ2nL.js
@@ -0,0 +1,1 @@
+const a="/portfolio/assets/Create%20and%20Design%20Digital%20Products%20using%20Canva-BzGGrUFL.jpg";export{a as default};
---
+++ assets/Introduction to Security Principles in Cloud Computing-B-FIu3n3.js
@@ -0,0 +1,1 @@
+const o="/portfolio/assets/Introduction%20to%20Security%20Principles%20in%20Cloud%20Computing-C3cdjOYE.jpg";export{o as default};
